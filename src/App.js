b407d4346517a2c1f395cf5d09d0994778d6502b
Athira K Sunny <athirabeenasunny@gmail.com>
2020-05-25 23:27:48 +1000

    Added menulist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 >
+          Welcome to my Page on { this.props.name }
+        </h1>
+        <h2><Menubar /></h2>
+
+      </div >
+    );
+  }
+}
+
+export class Menubar extends React.Component {
+  menulist = [ {
+    id: 1,
+    label: "Catalogue",
+  },
+  {
+    id: 2,
+    label: "Author List"
+  },
+  {
+    id: 3,
+    label: "Contact Us"
+  } ];
+  render() {
+    return (
+      <Menuitem items={ this.menulist }></Menuitem>
+    )
+  }
+}
+export class Menuitem extends React.Component {
+  render() {
+    return this.props.items.map(item => <li>{ item.label }</li>)
+  }
+
 }
 
 export default App;
